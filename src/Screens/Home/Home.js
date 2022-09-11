8b1942e6aefc4c2eefa30ea0fd47427bb747b6c8
9890ABHI <abhipawar7588@gmail.com>
2022-09-11 22:31:46 +0530

    new

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -87,7 +87,7 @@ export default function Home() {
                     height: 55,
                     position: 'relative',
                     top: 500,
-                    left: 50
+                    left: matches ? 50 :0,
                   }}
                 />
               </div>
