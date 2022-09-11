a9aed2b2cba67601cfc0efab372afd92926586ed
enryu-maker <akifkhan60067@gmail.com>
2022-09-11 11:56:56 +0530

    footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -25,7 +25,7 @@ function Footer() {
           paddingLeft: "10px",
         }} />
         <p style={{ ...FONTS.body4, paddingRight: "10px", }}>
-          &copy;{new Date().getFullYear()} <a href="https://nerdtech.in" target="_blank"  style={{ textDecoration: 'none', color: COLORS.black }} >NerdTech | All right reserved |</a><Link to={'/terms-and-condition'} style={{ textDecoration: 'none', color: COLORS.black }} > Terms & Service | Privacy</Link>
+          &copy;{new Date().getFullYear()} <a target="_blank"  style={{ textDecoration: 'none', color: COLORS.black }} >Grace and Peace LLC (AL) | All right reserved |</a><Link to={'/terms-and-condition'} style={{ textDecoration: 'none', color: COLORS.black }} > Terms & Service | Privacy</Link>
         </p>
 
       </footer></>
