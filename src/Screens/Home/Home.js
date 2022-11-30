8151a8d8e41cd8ecf360e9e33eec358c207f549d
9890ABHI <abhipawar7588@gmail.com>
2022-11-30 19:09:18 +0530

    home changes

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -48,9 +48,12 @@ export default function Home() {
         <Helmet>
           <meta charSet="utf-8" />
           <title>HerdHelp :: Herd Managament App</title>
-          <meta name="description" content="Herd Help is a farm documentation app that assist you in charting the herds activities.
+          <meta
+            name="description"
+            content="Herd Help is a farm documentation app that assist you in charting the herds activities.
 Documentation from insemination to medical records. Herd help does documentation for cows, sheep, goats, pigs, horses and rabbits.
-This allows you to grow a stronger healthier herd. Identifying profitable and unprofitable animals is the key to seeing profits." />
+This allows you to grow a stronger healthier herd. Identifying profitable and unprofitable animals is the key to seeing profits."
+          />
         </Helmet>
         <div style={{ backgroundColor: COLORS.white }}>
           <Slider />
@@ -63,13 +66,16 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
               // height: 600,
             }}
           >
-            <div style={{
-              display: "flex",
-              flexDirection: "column",
-              // justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-            }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                // justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                alignSelf: "center",
+              }}
+            >
               <h3
                 style={{
                   alignSelf: "flex-start",
@@ -87,7 +93,9 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                 description={"Track your herd's weight and growth"}
                 title2={"Herd's Health"}
                 image2={IMAGES.health}
-                description2={"HerdHelp allows you to keep track of your herd's health. You can keep track of your herd's medical records, vaccinations, and treatments."}
+                description2={
+                  "HerdHelp allows you to keep track of your herd's health. You can keep track of your herd's medical records, vaccinations, and treatments."
+                }
               />
               <Feature
                 title2={"Herd's Babies"}
@@ -95,7 +103,9 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                 description2={"Keep track of your herd's babies"}
                 title={"Set Alerts for your herd"}
                 image={IMAGES.alert}
-                description={"HerdHelp allows you to set alerts for your herd. You can set alerts for your herd's health, weight, and babies."}
+                description={
+                  "HerdHelp allows you to set alerts for your herd. You can set alerts for your herd's health, weight, and babies."
+                }
               />
               <Feature
                 title={"Control your Expenses"}
@@ -103,9 +113,10 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                 description={"Keep track of your herd's expenses"}
                 image2={IMAGES.profit}
                 title2={"Identify Profitable Animals"}
-                description2={"HerdHelp allows you to identify your herd's profitable animals."}
+                description2={
+                  "HerdHelp allows you to identify your herd's profitable animals."
+                }
               />
-
             </div>
           </div>
           <div
@@ -113,9 +124,10 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
               display: "flex",
               flexDirection: "column",
               // justifyContent: "center",
-              alignItems: "center",
+              // alignItems: "center",
               width: "100%",
-            }}>
+            }}
+          >
             <h3
               style={{
                 alignSelf: "flex-start",
@@ -128,36 +140,48 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
               About Us
             </h3>
             <div
-
               style={{
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "space-evenly",
                 width: "100%",
-              }}>
-            <p
-              style={{
-                color: COLORS.black,
-                textAlign: "left",
-                ...FONTS.body3,
-                width:"40%",
               }}
             >
-             My friend and I are both Christians and farmers in South Alabama. We both grew up here and both grew up raising cows. He still has cows and I mainly raise goats now. My background is computer software and technology and recently he has been taking programming classes. So you can tell how the two of us creating an APP to manage your livestock happened. We both understand technology and both understand animal husbandry. I decided to grow my goat herd to a larger scale and I needed the tools to do that in an organized manner. So here we are.
-              As business people, we know what we need to see a profit. With goats and sheep it’s kidding rates and with cows its weight gains. And with any animal that we raise we need healthy animals. If you track all your data you will have the tools to tell you which animals to cull and which animals are out producing the others. We hope that it helps you as much as it helps our farms.
-            </p>
-            <img
+              <p
+                style={{
+                  color: COLORS.black,
+                  textAlign: "left",
+                  ...FONTS.body3,
+                  width: "40%",
+                }}
+              >
+                My friend and I are both Christians and farmers in South
+                Alabama. We both grew up here and both grew up raising cows. He
+                still has cows and I mainly raise goats now. My background is
+                computer software and technology and recently he has been taking
+                programming classes. So you can tell how the two of us creating
+                an APP to manage your livestock happened. We both understand
+                technology and both understand animal husbandry. I decided to
+                grow my goat herd to a larger scale and I needed the tools to do
+                that in an organized manner. So here we are. As business people,
+                we know what we need to see a profit. With goats and sheep it’s
+                kidding rates and with cows its weight gains. And with any
+                animal that we raise we need healthy animals. If you track all
+                your data you will have the tools to tell you which animals to
+                cull and which animals are out producing the others. We hope
+                that it helps you as much as it helps our farms.
+              </p>
+              <img
                 src={IMAGES.about}
                 alt="feature1"
                 style={{
-                height: 350,
-                width: 350,
-                borderRadius: SIZES.radius,
+                  height: 350,
+                  width: 350,
+                  borderRadius: SIZES.radius,
                 }}
-            />
+              />
             </div>
           </div>
-
         </div>
 
         <div
@@ -168,7 +192,8 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
             alignItems: "center",
             width: "100%",
             paddingBottom: 50,
-          }}>
+          }}
+        >
           <h3
             style={{
               alignSelf: "flex-start",
@@ -180,15 +205,16 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
           >
             Tutorial videos
           </h3>
-          <ul style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            flexWrap: "wrap",
-          }}>
-
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              flexWrap: "wrap",
+            }}
+          >
             <FlatList
               list={videos}
               keyExtractor={(item) => `${item.id}`}
@@ -211,11 +237,7 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                     >
                       {item.title}
                     </p>
-                    <ReactPlayer
-                      height={250}
-                      width={400}
-
-                      url={item.link} />
+                    <ReactPlayer height={250} width={400} url={item.link} />
                   </div>
                 );
               }}
@@ -247,7 +269,8 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
           <div
             style={{
               display: "grid",
-              background: "linear-gradient(149.42deg, #D3F3D2 6.44%,rgba(5, 255, 0, 0.58) 58.02%, rgba(114, 242, 111, 0.81) 89.24%, rgba(4, 200, 0, 0.46) 136.74%);",
+              background:
+                "linear-gradient(149.42deg, #D3F3D2 6.44%,rgba(5, 255, 0, 0.58) 58.02%, rgba(114, 242, 111, 0.81) 89.24%, rgba(4, 200, 0, 0.46) 136.74%);",
             }}
           >
             <div style={{ width: "100%", height: "45vh", display: "flex" }}>
