8ca6c7d8e6c06ba9044143cd296be7018c4af5a0
enryu-maker <akifkhan60067@gmail.com>
2023-01-29 21:24:54 +0530

    c

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -104,7 +104,7 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                 title2={"Herd Health"}
                 image2={IMAGES.health}
                 description2={
-                      "Herd Help Livestock Management Software will help you by properly tracking each medication that you administer. It has a simple feature that allows you to treat and document actions on individual animals or on every goat in your herd with one easy step. No one wants to document that you gave Cydectin to each animal. We made it as simple as choosing each animal from the drop down and press add."
+                  "Herd Help Livestock Management Software will help you by properly tracking each medication that you administer. It has a simple feature that allows you to treat and document actions on individual animals or on every goat in your herd with one easy step. No one wants to document that you gave Cydectin to each animal. We made it as simple as choosing each animal from the drop down and press add."
                 }
               />
               <Feature
@@ -117,7 +117,7 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                   "Herd Help Livestock Management Software will assist you in understanding every aspect of your birthing. Proper livestock documentation will show you when your kidding will start taking place and track year over year the amount of babies each animal has. Soon, you will notice which animals give you healthier, more predictive babies. All the tools that you need for livestock records keeping on one simple device!"
                 }
               />
-              
+
             </div>
           </div>
           <div
@@ -289,62 +289,62 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                   }}
                 >
                   <div>
-                  <img
-                src={IMAGES.play}
-                alt={""}
-                onClick={() => {
-                  window.open(
-                    "https://play.google.com/store/apps/details?id=com.herdhelp",
-                    "_blank"
-                  );
-                }}
-                style={{
-                  height: 100,
-                  width: 120,
-                }}
-              />
-              <h3
-                  style={{
-                    alignSelf: "flex-start",
-                    color: COLORS.Primary,
-                    textAlign: "left",
-                    paddingInline: 10,
-                    ...FONTS.h3,
-                    letterSpacing: 1,
-                  }}
-                >
-                  Playstore
-                </h3>
+                    <img
+                      src={IMAGES.play}
+                      alt={""}
+                      onClick={() => {
+                        window.open(
+                          "https://play.google.com/store/apps/details?id=com.herdhelp",
+                          "_blank"
+                        );
+                      }}
+                      style={{
+                        height: 100,
+                        width: 120,
+                      }}
+                    />
+                    <h3
+                      style={{
+                        alignSelf: "flex-start",
+                        color: COLORS.Primary,
+                        textAlign: "left",
+                        paddingInline: 10,
+                        ...FONTS.h3,
+                        letterSpacing: 1,
+                      }}
+                    >
+                      Playstore
+                    </h3>
 
                   </div>
                   <div>
-              <img
-                src={IMAGES.app}
-                alt={""}
-                onClick={() => {
-                  window.open(
-                    "https://apps.apple.com/in/app/herdhelp/id1627766617",
-                    "_blank"
-                  );
-                }}
-                style={{
-                  height: 100,
-                  width: 100,
-                }}
-              /><h3
-              style={{
-                alignSelf: "flex-start",
-                color: COLORS.Primary,
-                textAlign: "left",
-                paddingInline: 10,
-                ...FONTS.h3,
-                letterSpacing: 1,
-              }}
-            >
-              Appstore
-            </h3>
-            </div>
-                  
+                    <img
+                      src={IMAGES.app}
+                      alt={""}
+                      onClick={() => {
+                        window.open(
+                          "https://apps.apple.com/in/app/herdhelp/id1627766617",
+                          "_blank"
+                        );
+                      }}
+                      style={{
+                        height: 100,
+                        width: 100,
+                      }}
+                    /><h3
+                      style={{
+                        alignSelf: "flex-start",
+                        color: COLORS.Primary,
+                        textAlign: "left",
+                        paddingInline: 10,
+                        ...FONTS.h3,
+                        letterSpacing: 1,
+                      }}
+                    >
+                      Appstore
+                    </h3>
+                  </div>
+
                 </div>
                 <h3
                   style={{
@@ -368,36 +368,36 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                   }}
                 >
                   <Feature
-                title2={"Control your Expenses"}
-                image2={IMAGES.finance}
-                description2={"Keep track of your herd expenses"}
-                image={IMAGES.profit}
-                title={"Identify Profitable Animals"}
-                description={
-                  "HerdHelp allows you to identify your herd profitable animals."
-                }
-              />
+                    title2={"Control your Expenses"}
+                    image2={IMAGES.finance}
+                    description2={"Herd Help Livestock Management Software has specific places to track your expenses by category. This gives you the ability to see where you are spending your money. For example feed, fencing, medications, etc. Understanding your expenses will make life easier! Our livestock management documentation software should help you manage each piece of your farm program better."}
+                    image={IMAGES.profit}
+                    title={"Identify Profitable Animals"}
+                    description={
+                      "When proper livestock record keeping is kept on every animal, you start to understand if your livestock are making you a profit. My business partner shipped cows twice before he remembered to ship one cow that had prolapsed on him. This means that he fed her for two years longer than he needed. She produced zero profit in those two years. In our app we have a place to flag certain animals so these scenarios do not take place. When I am raising goats, I am looking for twins from each animal every year. With the goat management software, I can see twins, plus I can see how often I am dealing with parasite pressure. The same would apply to sheep. If I am using our sheep management software to show me which animals gave me only singles and had constant health issues, then I know which sheep to cull, or if I need to change my buck because it's a global issue. If I am looking at each aspect of what my farm produces income from, then I am going to identify profits and losses. It’s as simple as good livestock record keeping."
+                    }
+                  />
                   <Feature
                     title={"Herd Weight"}
                     image={IMAGES.weight1}
-                    description={"Track your herd weight and growth"}
+                    description={"Herd Help Livestock Management Software will help you track birth weights and daily gains. We don’t care which animals you raise, whether it’s goats, cattle, sheep, pigs, horses, or rabbits, you will see better livestock come from your farm if you know and understand how to increase weights. This is why it was important for us to create an app that not only is for goat records keeping, but also does your cattle record keeping and everything in between. Even our efforts raising rabbits on our farm is better understood when we properly keep better rabbit records."}
                     title2={"Herd Health"}
                     image2={IMAGES.health}
                     description2={
-                      "HerdHelp allows you to keep track of your herd health. You can keep track of your herd medical records, vaccinations, and treatments."
+                      "Herd Help Livestock Management Software will help you by properly tracking each medication that you administer. It has a simple feature that allows you to treat and document actions on individual animals or on every goat in your herd with one easy step. No one wants to document that you gave Cydectin to each animal. We made it as simple as choosing each animal from the drop down and press add."
                     }
                   />
                   <Feature
                     title2={"Birthing"}
                     image2={IMAGES.babies}
-                    description2={"Keep track of your herd babies and births"}
+                    description={"Herd Help Livestock Management Software has a key feature that allows you to set reminders. If you document that goat number 150 gets bred on Oct 2, you can then set a reminder to alert you in 145 days to start watching for your babies. This is where proper livestock records keeping will play a big part in better farm management."}
                     title={"Set Alerts for your Herd"}
                     image={IMAGES.alert}
-                    description={
-                      "HerdHelp allows you to set alerts for your herd. You can set alerts for your herd health, weight, and babies."
+                    description2={
+                      "Herd Help Livestock Management Software will assist you in understanding every aspect of your birthing. Proper livestock documentation will show you when your kidding will start taking place and track year over year the amount of babies each animal has. Soon, you will notice which animals give you healthier, more predictive babies. All the tools that you need for livestock records keeping on one simple device!"
                     }
                   />
-                  
+
                 </div>
               </div>
               <div
@@ -449,22 +449,7 @@ This allows you to grow a stronger healthier herd. Identifying profitable and un
                       paddingInline: 10,
                     }}
                   >
-                    My friend and I are both Christians and farmers in South
-                    Alabama. We both grew up here and both grew up raising cows.
-                    He still has cows and I mainly raise goats now. My
-                    background is computer software and technology and recently
-                    he has been taking programming classes. So you can tell how
-                    the two of us creating an APP to manage your livestock
-                    happened. We both understand technology and both understand
-                    animal husbandry. I decided to grow my goat herd to a larger
-                    scale and I needed the tools to do that in an organized
-                    manner. So here we are. As business people, we know what we
-                    need to see a profit. With goats and sheep it’s kidding
-                    rates and with cows its weight gains. And with any animal
-                    that we raise we need healthy animals. If you track all your
-                    data you will have the tools to tell you which animals to
-                    cull and which animals are out producing the others. We hope
-                    that it helps you as much as it helps our farms.
+                    My friend and I needed a livestock software program for our farms so that we could accurately track our animals, their health, and profitability. We tried other apps, software programs, and spreadsheets, to help with the record keeping but we couldn’t find a livestock management app that was easy to use and fit our needs. So, we created a piece of software that would track any animals that we might bring to our farm, whether it be cattle, sheep, goats, rabbits, pigs, or horses. The app was created to work on Android or Iphone cellular devices as well as on your laptop. We needed a product that was available while in the field and a larger version to see for reporting back at home. The cattle management software was created for my buddy who primarily raises cows. I needed goat management software for my goats and sheep management software in case my neighbor needed help. But, we have also been known to keep other animals on the farm occasionally. We added in pig management, rabbit management, and horse management tools to make it a complete farm software program. Why create a cattle app when you raise goats, cattle, sheep, pigs, rabbits, and horses? Well, when you think about a homestead, you realize that most small farms will at one point have all of these animals and each of them needs a record keeping software program to help get them through tough decisions.
                   </p>
                 </div>
               </div>
